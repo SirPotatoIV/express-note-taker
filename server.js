522e79ea891f7c8e2ebb3d9d22dc091fa8fa80be
SirPotatoIV <otoolej33@gmail.com>
2019-12-13 19:29:35 -0600

    Realized code was constantly returning errors in the console.log turns out html could not access the style sheet or the javascript. The issue is due to running the server locally. Added code to pass the needed code to the html when testing locally. Added a get for the already existing notes.

--- server.js
+++ server.js
@@ -14,13 +14,44 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Create a set of routes for displaying the HTML pages
+// =========================================
+// * GET `/notes` - Should return the `notes.html` file.
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+// * GET `*` - Should return the `index.html` file
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
-app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "public/notes.html"));
+// ONLY BEING ADDED FOR LOCAL TESTING PURPOSES!!!!---------------------
+if(PORT === 3000){
+    // * GET `*` - Should return the `index.html` file
+    app.get("/assets/*", function(req, res) {
+        res.sendFile(path.join(__dirname, `/public/${req.url}`))
+        console.log("annoying issue occurred")
+        console.log(req.url);
+    });
+
+}
+// --------------------------------------------------------------------
+
+
+// Create New Reservation - takes in JSON input
+//   * GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
+app.get("/api/notes", function(req, res) {
+    
+    // console.log(testArray)
+    return res.json([{"title":"Test Title","text":"Test text"}]);
 });
+// * The following API routes should be created:
+
+
+//   * POST `/api/notes` - Should recieve a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
+
+//   * DELETE `/api/notes/:id` - Should recieve a query paramter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
+
+
 
 
 // Starts the server to begin listening
